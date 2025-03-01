160049a1b75e0532971723c7052585e6ea6c8947
oleg <ip.pan@bk.ru>
2025-03-01 19:50:59 +0400

    KS-1-3 practice 3 smth

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,5 +1,20 @@
 package org.example.lesson_1
 
 fun main () {
-    println("Тестовое решение задач к уроку 1 задаче 2")
+    println("Тестовое решение задач к уроку 1 задаче 3")
+
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
+
+    print("Взлет ")
+    print("$year ")
+    print(hour)
+    print(":")
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    println("Посадка совершена $year $hour:$minute")
 }
