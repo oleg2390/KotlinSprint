1d3261c41f6129f04868848974bde1b222a38e7e
oleg <ip.pan@bk.ru>
2025-03-14 02:45:36 +0400

    KS-4-3 test 3 do smth

---
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_4
+
+const val DEFAULT_SUNNY = true
+const val DEFAULT_TENT = true
+const val DEFAULT_PERCENT = 20
+const val DEFAULT_SEASON = "не зима"
+fun main () {
+
+    val sunny: Boolean = true
+    val tentOpen: Boolean = true
+    val percent: Int = 20
+    val season: String = "зима"
+
+    println("Благоприятные ли условия сейчас для роста бобовых? ${
+        DEFAULT_SUNNY == sunny &&
+        DEFAULT_TENT == tentOpen &&
+        DEFAULT_PERCENT == percent &&
+        DEFAULT_SEASON == season
+    }")
+}
