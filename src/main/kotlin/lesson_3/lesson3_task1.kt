1b96b1fc10c8f7b28d82ab12bc7b704271d7af2e
oleg <ip.pan@bk.ru>
2025-03-14 00:34:48 +0400

    KS-2-4 test 4 refactor

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+fun main () {
+
+    var greeting: String
+    val userName = "Иван"
+
+    greeting = "Добрый день"
+    println("$greeting, $userName")
+
+    greeting = "Добрый вечер"
+    println("$greeting, $userName")
+}
