9faef5983eeaa42c8b9ef48800d6cc24c168a9b3
oleg <ip.pan@bk.ru>
2025-03-14 00:31:13 +0400

    KS-2-4 test 4 refactor

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,13 +1,14 @@
 package org.example.lesson_2
 
+const val NUMBER_100 = 100
 fun main () {
 
     val crystalOre = 7
     val ironOre = 11
-    val buffMultiplier = 0.2
+    val buffMultiplier = 20
 
-    val crystalBonus = (crystalOre * buffMultiplier).toInt()
-    val ironBonus = (ironOre * buffMultiplier).toInt()
+    val crystalBonus = (crystalOre * buffMultiplier / NUMBER_100 )
+    val ironBonus = (ironOre * buffMultiplier / NUMBER_100 )
 
     println("Кристалическая руда: $crystalBonus")
     println("Железная руда: $ironBonus")
