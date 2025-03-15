33d7c496166a4d9a659ef001fb348b1b4c872c53
oleg <ip.pan@bk.ru>
2025-03-16 00:02:49 +0400

    KS-2-5 test 5 refactor conflict

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -2,14 +2,14 @@ package org.example.lesson_2
 
 import kotlin.math.pow
 
-const val NUMBER_100 = 100
+const val NUMBE_100 = 100
 fun main () {
 
     val bankClient = 70_000.0
     val procentRate = 16.7
     val years = 20.0
 
-    val summaInBankClient = bankClient * (1 + (procentRate / NUMBER_100)).pow(years)
+    val summaInBankClient = bankClient * (1 + (procentRate / NUMBE_100)).pow(years)
     val formatedSumma = String.format("Сумма на конец срока: %.3f", summaInBankClient)
 
     println(formatedSumma)
