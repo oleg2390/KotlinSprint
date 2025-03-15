7e8bf01d0e9323d776f82605e6e94d339ee761f0
oleg <ip.pan@bk.ru>
2025-03-15 23:33:49 +0400

    KS-4-1 test 1 refactor

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -6,6 +6,6 @@ fun main () {
     val numberOfTablesBookedToday = 13
     val numberOfTablesBookedTomorrow = 9
 
-    println("[Доступность столиков на сегодня: ${numberOfTablesBookedToday != MAX_TABLES }]\n" +
-            "[Доступность столиков на завтра: ${numberOfTablesBookedTomorrow != MAX_TABLES }]")
+    println("[Доступность столиков на сегодня: ${numberOfTablesBookedToday < MAX_TABLES }]\n" +
+            "[Доступность столиков на завтра: ${numberOfTablesBookedTomorrow < MAX_TABLES }]")
 }
