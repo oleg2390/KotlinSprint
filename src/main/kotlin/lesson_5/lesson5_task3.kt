e098d024c3d3cff87d80b5ace82d6bcaa2d0511e
oleg <ip.pan@bk.ru>
2025-03-16 02:29:33 +0400

    KS-5-3 test 3 do smth

---
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -0,0 +1,30 @@
+package org.example.lesson_5
+
+
+fun main() {
+    val firstNumberWin = 4
+    val secondNumberWin = 33
+
+    println("лотерея, нужно угадать два числа от 0 до 42")
+    println("Введите первое число от 0 до 42")
+
+    val firstNumberUser = readln().toInt()
+
+    println("Введите второе число от 0 до 42")
+
+    val secondNumberUser = readln().toInt()
+
+    when {
+        (firstNumberUser == firstNumberWin && secondNumberUser == secondNumberWin) ||
+                (secondNumberUser == firstNumberWin && firstNumberUser == secondNumberWin) ->
+            println("Поздравляем! Вы выиграли главный приз!")
+
+        (firstNumberUser == firstNumberWin && secondNumberUser != secondNumberWin) ||
+                (firstNumberUser != firstNumberWin || secondNumberWin == secondNumberWin) ->
+            println("Вы выиграли утешительный приз!")
+
+        else -> println("Неудача!")
+    }
+
+    println("числа были нужны для победы $firstNumberWin и $secondNumberWin")
+}
