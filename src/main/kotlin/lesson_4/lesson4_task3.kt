184cf039a7806a8fa7d60adb7a28f925cf05275b
oleg <ip.pan@bk.ru>
2025-03-15 23:26:31 +0400

    KS-4-3 test 3 refactor

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -3,7 +3,7 @@ package org.example.lesson_4
 const val DEFAULT_SUNNY = true
 const val DEFAULT_TENT = true
 const val DEFAULT_PERCENT = 20
-const val DEFAULT_SEASON = "не зима"
+const val DEFAULT_SEASON = "зима"
 fun main () {
 
     val sunny: Boolean = true
@@ -15,6 +15,6 @@ fun main () {
         DEFAULT_SUNNY == sunny &&
         DEFAULT_TENT == tentOpen &&
         DEFAULT_PERCENT == percent &&
-        DEFAULT_SEASON == season
+        DEFAULT_SEASON != season
     }")
 }
