664fbdd962f1b2a404ee7b25ab83312d36a7f9ee
oleg <ip.pan@bk.ru>
2025-03-15 23:21:08 +0400

    KS-3-2 test 2 refactor

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -4,13 +4,13 @@ fun main () {
 
     val fistName = "Татьяна"
     val patronymic = "Сергеевна"
-    val surname = "Андреева"
-    val newSurname = "Сидорова"
+    var surname = "Андреева"
     var age20 = 20
     var age22 = 22
 
     val name20 = "$surname $fistName $patronymic"
-    val name22 = "$newSurname $fistName $patronymic"
+    surname = "Сидорова"
+    val name22 = "$surname $fistName $patronymic"
 
     println("$name20, $age20")
     println("$name22, $age22")
