ee48b446b21b66dc69623d046e67d77cf5ac6533
oleg <ip.pan@bk.ru>
2025-03-17 00:36:43 +0400

    KS-4-3 test 3 change of surname refactor

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -5,13 +5,10 @@ fun main () {
     val fistName = "Татьяна"
     val patronymic = "Сергеевна"
     var surname = "Андреева"
-    var age20 = 20
-    var age22 = 22
+    var age = 20
 
-    val name20 = "$surname $fistName $patronymic"
+    println("$surname, $fistName, $patronymic, $age")
+    age = 22
     surname = "Сидорова"
-    val name22 = "$surname $fistName $patronymic"
-
-    println("$name20, $age20")
-    println("$name22, $age22")
+    println("$surname, $fistName, $patronymic, $age")
 }
