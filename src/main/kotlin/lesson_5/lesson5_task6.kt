5964c699eb9d8a8eddc1d762221332e14a1b9ca5
oleg <ip.pan@bk.ru>
2025-03-17 01:43:04 +0400

    KS-5-6 test 6 imt create

---
+++ src/main/kotlin/lesson_5/lesson5_task6.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_5
+
+import kotlin.math.pow
+
+fun main() {
+
+    println("Введите свой вес в кг:")
+    val userWeight = readln().toDouble()
+    println("Введите свой рост в сантиметрах:")
+    val userGrowth = readln().toDouble()
+
+    val imt = userWeight / (userGrowth / 100).pow(2.0)
+    when {
+        imt < 18.5 -> println("Недостаточная масса тела")
+        imt in 18.5..<25.0 -> println("Нормальная масса тела")
+        imt in 25.0..<30.0 -> println("Избыточная масса тела")
+        imt >= 30.0 -> println("Ожирение")
+    }
+}
