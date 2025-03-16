4a9bba8924a40bfa4a65c79aa5faa31101514961
oleg <ip.pan@bk.ru>
2025-03-17 00:23:02 +0400

    KS-4-3 test 3 refactor names constants

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,19 +1,19 @@
 package org.example.lesson_4
 
-const val DEFAULT_SUNNY = true
-const val DEFAULT_TENT = true
+const val IS_SUNNY = true
+const val IS_TENT = true
 const val DEFAULT_PERCENT = 20
 const val DEFAULT_SEASON = "зима"
 fun main () {
 
-    val sunny: Boolean = true
-    val tentOpen: Boolean = true
+    val isSunny: Boolean = true
+    val isTentOpen: Boolean = true
     val percent: Int = 20
     val season: String = "зима"
 
     println("Благоприятные ли условия сейчас для роста бобовых? ${
-        DEFAULT_SUNNY == sunny &&
-        DEFAULT_TENT == tentOpen &&
+        IS_SUNNY == isSunny &&
+        IS_TENT == isTentOpen &&
         DEFAULT_PERCENT == percent &&
         DEFAULT_SEASON != season
     }")
