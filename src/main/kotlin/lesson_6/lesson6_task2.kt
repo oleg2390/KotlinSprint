6eb0157d446426db2129a3754be38715d376ac89
oleg <ip.pan@bk.ru>
2025-03-20 01:25:41 +0400

    KS-6-2 test 2 create timer

---
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_6
+
+fun main() {
+    println("количество секунд, которые нужно засечь?")
+    var userInput = readln().toInt()
+
+    var timer = userInput
+
+    while (timer > 0) {
+        timer--
+        Thread.sleep(1000)
+    }
+    println("Прошло $userInput секунд")
+}
