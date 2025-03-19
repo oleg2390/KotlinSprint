c6a0afba77e2a12fb4e9381f70bb92b18c5da03b
oleg <ip.pan@bk.ru>
2025-03-19 23:14:56 +0400

    KS-5-6 test 6 imt refactor

--- src/main/kotlin/lesson_5/lesson5_task6.kt
+++ src/main/kotlin/lesson_5/lesson5_task6.kt
@@ -12,8 +12,8 @@ fun main() {
     val imt = userWeight / (userGrowth / 100).pow(2.0)
     when {
         imt < 18.5 -> println("Недостаточная масса тела")
-        imt in 18.5..<25.0 -> println("Нормальная масса тела")
-        imt in 25.0..<30.0 -> println("Избыточная масса тела")
+        imt < 25.0 -> println("Нормальная масса тела")
+        imt < 30.0 -> println("Избыточная масса тела")
         imt >= 30.0 -> println("Ожирение")
     }
 }
