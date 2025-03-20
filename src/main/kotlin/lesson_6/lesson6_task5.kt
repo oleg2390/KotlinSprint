b7f03b4b523ae65eac2571e78cf5179637f9f99a
oleg <ip.pan@bk.ru>
2025-03-21 00:43:36 +0400

    KS-6-5 test 5 refactor check for the bot

--- src/main/kotlin/lesson_6/lesson6_task5.kt
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -1,14 +1,12 @@
 package org.example.lesson_6
 
-import kotlin.random.Random
-
 fun main() {
 
     var countАttempt = 3
 
     while (countАttempt > 0) {
-        val randomNumberFirst = Random.nextInt(1, 9)
-        val randomNumberSecond = Random.nextInt(1, 9)
+        val randomNumberFirst = (1.. 9).random()
+        val randomNumberSecond = (1.. 9).random()
         val result = randomNumberSecond + randomNumberFirst
 
         println("Напишите сумму чисел $randomNumberFirst + $randomNumberSecond если вы не бот")
