1db47df3a9671bd11419e29fc4f4c7217517da1f
oleg <ip.pan@bk.ru>
2025-03-21 02:12:39 +0400

    KS-6-2 test 2 refactor timer and add user input in Thread

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -1,12 +1,12 @@
 package org.example.lesson_6
 
+const val TIMER = 1000
 fun main() {
     println("количество секунд, которые нужно засечь?")
     var userInput = readln().toInt()
 
-    var timer = userInput
-    timer--
-    Thread.sleep(1000)
+    var timer = userInput * TIMER
+    Thread.sleep(timer.toLong())
 
     println("Прошло $userInput секунд")
 }
