65942ca3e9f6833aa80ed0604bac8b5a086ef837
oleg <ip.pan@bk.ru>
2025-03-21 02:22:51 +0400

    KS-5-5 test 5 add constants

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -1,19 +1,21 @@
 package org.example.lesson_5
 
+const val RANGE_NUMBER_1 = 0
+const val RANGE_NUMBER_2 = 42
 fun main() {
 
     val numberD = List(3) {(1..42).random()}
     val userNumbers = mutableSetOf<Int>()
 
-    println("Введите 3 числа от 0 до 42")
+    println("Введите 3 числа от $RANGE_NUMBER_1 до $RANGE_NUMBER_2")
 
     while (userNumbers.size < 3) {
         println("Введите число ${userNumbers.size + 1}/3")
         val input = readln().toInt()
 
-        if (input in 0..42) {
+        if (input in RANGE_NUMBER_1..RANGE_NUMBER_2) {
             userNumbers.add(input)
-        } else println("Не корректный ввод, введите еще раз число от 0..42")
+        } else println("Не корректный ввод, введите еще раз число от $RANGE_NUMBER_1..$RANGE_NUMBER_2")
     }
 
     val matches = userNumbers.intersect(numberD).size
