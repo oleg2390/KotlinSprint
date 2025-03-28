83d63a0bff9df79602dfc79a4267c3905e6ba3af
oleg <ip.pan@bk.ru>
2025-03-29 00:50:00 +0400

    KS-12-4 test 4 create weather refactor

--- src/main/kotlin/lesson_12/lesson12_task4.kt
+++ src/main/kotlin/lesson_12/lesson12_task4.kt
@@ -1,8 +1,9 @@
 package org.example.lesson_12
 
-fun main() {
-    val weatherInf = WeatherInf(290, 300, false).printWeather()
+const val CELS1 = 273.15
 
+fun main() {
+    val weatherInf = WeatherInf(320, 300, false).printWeather()
 }
 
 class WeatherInf(
@@ -11,15 +12,15 @@ class WeatherInf(
     precipitationDay: Boolean,
 ) {
 
-    private var _daytimeTemperature: Int = daytimeTemperature
-    private var _nightTemperature: Int = nightTemperature
+    private var _daytimeTemperature = daytimeTemperature - CELS1
+    private var _nightTemperature = nightTemperature - CELS1
     private var _precipitationDay: Boolean = precipitationDay
 
     fun printWeather() {
         println(
             """
-            ${String.format("погода днем: %.2f", _daytimeTemperature - 273.15)} C
-            ${String.format("погода ночью: %.2f", _nightTemperature - 273.15)} C
+            ${String.format("погода днем: %.2f", _daytimeTemperature )} C
+            ${String.format("погода ночью: %.2f", _nightTemperature )} C
             возможны осадки - $_precipitationDay
         """.trimIndent()
         )
