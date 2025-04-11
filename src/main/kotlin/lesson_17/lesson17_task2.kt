adb6b4ac7cb6931116d84b67f476e7a44abdcc73
oleg <ip.pan@bk.ru>
2025-04-11 22:36:36 +0400

    KS-17-2 test 2 create ship and block refactor name

---
+++ src/main/kotlin/lesson_17/lesson17_task2.kt
@@ -0,0 +1,22 @@
+package org.example.lesson_17
+
+fun main() {
+
+    val ship = Ship()
+    println(ship.nameShip1)
+    ship.nameShip1 = "oleg"
+}
+
+class Ship(
+
+    nameShip1: String = "KING",
+    speed: Int = 23,
+    portRegistration: String = "2341",
+) {
+    var nameShip1 = nameShip1
+        get() = field
+        set(value) {
+            println("нельзя менять")
+            field = value
+        }
+}
