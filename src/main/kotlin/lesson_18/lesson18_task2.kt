b23089bc24ba44f76920d6b5e1a00594dacca210
oleg <ip.pan@bk.ru>
2025-04-13 13:40:20 +0400

    KS-18-2 test 2 refactor

--- src/main/kotlin/lesson_18/lesson18_task2.kt
+++ src/main/kotlin/lesson_18/lesson18_task2.kt
@@ -4,9 +4,9 @@ private const val NUMBER_1 = 1
 
 fun main() {
 
-    val d4 = Cubik4(4)
-    val d6 = Cubik6(6)
-    val d8 = Cubik8(8)
+    val d4 = Cubik4()
+    val d6 = Cubik6()
+    val d8 = Cubik8()
 
     val listCubik = listOf<Cubik>(d4, d6, d8)
 
@@ -25,8 +25,8 @@ open class Cubik(
     }
 }
 
-class Cubik4(grani: Int) : Cubik(grani)
+class Cubik4(override val grani: Int = 4) : Cubik(grani)
 
-class Cubik6(grani: Int) : Cubik(grani)
+class Cubik6(override val grani: Int = 6) : Cubik(grani)
 
-class Cubik8(grani: Int) : Cubik(grani)
+class Cubik8(override val grani: Int = 8) : Cubik(grani)
