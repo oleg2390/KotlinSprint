47dc4ec89f1189dc78848af3333ec1e1eb48220f
oleg <ip.pan@bk.ru>
2025-04-16 21:22:38 +0400

    KS-20-4 test 4 refactor lambda

--- src/main/kotlin/lesson_20/lesson20_task4.kt
+++ src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -2,13 +2,13 @@ package org.example.lesson_20
 
 fun main() {
 
-    val listElements1 = listOf("element1", "element2", "element3", "element3")
+    val listElements1 = listOf("element1", "element2", "element3", "element4")
 
-    val actions: List<() -> Unit> = listElements1.map { element ->
-        { println("Нажат элемент - $element") }
+    val actions: List<() -> Unit> = listElements1
+        .withIndex()
+        .filter {it.index % 2 == 1}
+        .map { element ->
+        { println("Нажат элемент - ${element.value}") }
     }
-
-    for (i in actions.indices step 2) {
-        actions[i]()
-    }
+    actions.forEach {it()}
 }
